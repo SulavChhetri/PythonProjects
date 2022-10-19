4d149f0f338d0b4a3816aff90332d2ced66abe7e
Xulav <sulavthapachhetri@gmail.com>
2022-10-19 13:10:20 +0545

    final

--- parser/main.py
+++ parser/main.py
@@ -7,13 +7,15 @@ def main():
         lines = [line.rstrip('\n') for line in file]
         lines = lines[5:]
     for item in lines:
+        y = 0
         x = item.split("    ")
         for i in range(int(len(x)/3)):
             filecontainer.append({
-            'State': x[i],
-            'Postal Abbr': x[i+1],
-            'FIPS Code': x[i+2]})
-
+            'State': x[y],
+            'Postal Abbr': x[y+1],
+            'FIPS Code': x[y+2]})
+            y=3
+            
     with open('parsed.json', 'w')as file:
         json.dump(filecontainer, file)
 
